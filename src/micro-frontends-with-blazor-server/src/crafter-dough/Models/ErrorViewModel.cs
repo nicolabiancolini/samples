c60bd2517372a988edd0f55b12298c9d0a253095
Nicola Biancolini <nicola.biancolini@gmail.com>
2022-05-06 14:54:20 +0200

    Micro-frontends with Blazor Server (#16)
    * Demostrate how use routable RCL with MVC
    
    * Exclude publishing profiles from source version control

---
+++ src/micro-frontends-with-blazor-server/src/crafter-dough/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Crafter.Dough.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
